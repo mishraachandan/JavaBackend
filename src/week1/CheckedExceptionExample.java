eac5cff218053e87ed15c0eec8d3aaa3d1c2a445
Chandan Mishra <chandan345mishra@gmail.com>
2023-11-13 23:13:11 +0530

    Codium suggestion changes.

--- src/week1/CheckedExceptionExample.java
+++ src/week1/CheckedExceptionExample.java
@@ -1,25 +1,17 @@
 package week1;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class CheckedExceptionExample {
 
     public static void main(String[] args) throws CustomException {
+        checkFileExists("nonexistent.txt");
+    }
 
-        // Printing the exact message and not including the detailed message.
-        File file = new File("nonexistent.txt");
-        try{
-            if(!file.exists()){
-                throw new CustomException("File not found. Give a correct path.");
-            }
+    public static void checkFileExists(String filePath) throws CustomException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            throw new CustomException("File not found. Give a correct path.");
         }
-        catch (CustomException e){
-            System.out.println(e.getMessage());
-        }
-
-
     }
 }
